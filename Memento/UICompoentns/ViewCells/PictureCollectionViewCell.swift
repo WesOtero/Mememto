554d4a205c47b86bafc17af758c9edba22cdcefe
Wes <wesleyotero@protonmail.com>
2020-05-24 11:48:03 -0400

    Adds data models for Post and UserProfile

---
+++ Memento/UICompoentns/ViewCells/PictureCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PictureCollectionViewCell.swift
+//  Memento
+//
+//  Created by Wesley Otero on 5/24/20.
+//  Copyright © 2020 Wesley Otero. All rights reserved.
+//
+
+import UIKit
+
+class PictureCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+}
