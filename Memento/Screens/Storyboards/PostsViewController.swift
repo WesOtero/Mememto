0a3e5279a220065abd74f8d277f5e8377eb4a361
Wes <wesleyotero@protonmail.com>
2020-05-23 23:53:14 -0400

    Updates to UI

--- Memento/Screens/Storyboards/PostsViewController.swift
+++ Memento/Screens/Storyboards/PostsViewController.swift
@@ -12,9 +12,12 @@ class PostsViewController: UIViewController {
     
     
     @IBOutlet weak var titleNavBarItem: UINavigationItem!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         configureNavBar()
     }
     
@@ -22,5 +25,19 @@ class PostsViewController: UIViewController {
         navigationController?.setNavigationBarHidden(true, animated: true)
         titleNavBarItem.title = self.title
     }
+    
+}
+
+extension PostsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
     
 }
